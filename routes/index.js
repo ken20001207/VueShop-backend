1fc52bfe35ef25359d3377349ddc1a667da7243e
ken20001207 <varedx@gmail.com>
2019-11-27 14:21:43 +0800

    first upload

---
+++ routes/index.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+// Example Databse : Yuanlin's Aliyun RDS Database #1
+var connection = mysql.createConnection({
+    host: 'rm-bp1g8y2t596rw3c2i3o.mysql.rds.aliyuncs.com',
+    user: 'yuanlin',
+    password: 'Yuanlin1207!'
+});
+
+/* Get all items. */
+router.get('/items', function(req, res, next) {
+
+    var first = 1;
+
+    connection = mysql.createConnection(connection.config);
+    connection.connect();
+
+    connection.query('SELECT * FROM `vueshop_demo`.`items`;', function(err, rows, fields) {
+
+        res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+        res.write("{\"items\":[");
+
+        Object.keys(rows).forEach(function(key) {
+
+            var row = rows[key];
+
+            if (!first) {
+                res.write(",");
+            } else {
+                first = 0;
+            }
+
+            res.write(row.data);
+        });
+        res.write("]}");
+        res.end();
+    });
+
+    connection.end();
+});
+
+/* Get all classes. */
+router.get('/classes', function(req, res, next) {
+
+    var first = 1;
+
+    connection = mysql.createConnection(connection.config);
+    connection.connect();
+
+    connection.query('SELECT * FROM `vueshop_demo`.`classes`;', function(err, rows, fields) {
+
+        res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
+        res.write("{\"classes\":[");
+
+        Object.keys(rows).forEach(function(key) {
+
+            var row = rows[key];
+
+            if (!first) {
+                res.write(",");
+            } else {
+                first = 0;
+            }
+
+            res.write(row.data);
+        });
+        res.write("]}");
+        res.end();
+    });
+
+    connection.end();
+});
+
+module.exports = router;
